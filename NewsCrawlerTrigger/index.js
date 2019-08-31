979744a37366d8b290df5c802695396f1647078b
Suraj Shrestha <syuraj@gmail.com>
2019-08-31 12:51:21 -0400

    NEP-26 Ignore error during scraping

--- NewsCrawlerTrigger/index.js
+++ NewsCrawlerTrigger/index.js
@@ -1,61 +1,56 @@
 module.exports = async function(context, myTimer) {
-  var timeStamp = new Date().toISOString();
-  const { newsDbService } = require("nepaltoday-db-service");
-  const { scrapeNewsLink } = require("./linkCrawler");
-  const { scrapeNewsContent } = require("./ContentCrawler");
+	var timeStamp = new Date().toISOString()
+	const { newsDbService } = require('nepaltoday-db-service')
+	const { scrapeNewsLink } = require('./linkCrawler')
+	const { scrapeNewsContent } = require('./ContentCrawler')
 
-  const ipAddress = require("ip").address();
+	const ipAddress = require('ip').address()
 
-  if (myTimer.IsPastDue) {
-    context.log("JavaScript is running late!");
-  }
+	if (myTimer.IsPastDue) {
+		context.log('JavaScript is running late!')
+	}
 
-  try {
-    const sources = await newsDbService.getAllSources();
-    if (sources) {
-      sources.map(async source => {
-        const sourceId = source._id;
-        const baseUrl = source.link;
-        const logoLink = source.logoLink;
-        const categories = source.category;
-        if (categories) {
-          categories.map(async category => {
-            const categoryName = category.name;
-            const url = `${baseUrl}${category.path}`;
-            const { error, data } = await scrapeNewsLink(baseUrl, url);
-            if (error) {
-              throw new Error(error);
-            }
-            if (Array.isArray(data) && data.length > 0) {
-              //   console.log("news link", newsLinks);
-              data.map(async link => {
-                const content = await scrapeNewsContent(
-                  `${link.url}`,
-                  logoLink
-                );
-                if (content && content.title && sourceId) {
-                  content.source = sourceId;
-                  content.createdDate = new Date();
-                  content.modifiedDate = new Date();
-                  content.isHeadline = true; // TODO: check if h1 or h2
-                  content.hostIp = ipAddress;
-                  content.category = categoryName;
-                  //   console.log("content", content);
-                  const savedArticle = await newsDbService.saveArticle(content);
-                  if (savedArticle) {
-                    context.log("article saved successfully!!!!");
-                  }
-                }
-              });
-            }
-          });
-        }
-      });
-    }
-  } catch (error) {
-    context.log("error occured here", error);
-  }
+	try {
+		const sources = await newsDbService.getAllSources()
+		if (sources) {
+			sources.map(async source => {
+				const sourceId = source._id
+				const baseUrl = source.link
+				const logoLink = source.logoLink
+				const categories = source.category
 
-  context.log("JavaScript timer trigger function ran!", timeStamp);
-  context.done();
-};
+				if (categories) {
+					categories.map(async category => {
+						const categoryName = category.name
+						const url = `${baseUrl}${category.path}`
+						const { data } = await scrapeNewsLink(baseUrl, url)
+
+						if (Array.isArray(data) && data.length > 0) {
+							//   console.log("news link", newsLinks);
+							data.map(async link => {
+								const content = await scrapeNewsContent(`${link.url}`, logoLink)
+								if (content && content.title && sourceId) {
+									content.source = sourceId
+									content.createdDate = new Date()
+									content.modifiedDate = new Date()
+									content.isHeadline = true // TODO: check if h1 or h2
+									content.hostIp = ipAddress
+									content.category = categoryName
+									//   console.log("content", content);
+									const savedArticle = await newsDbService.saveArticle(content)
+									if (savedArticle) {
+										context.log('article saved successfully!!!!')
+									}
+								}
+							})
+						}
+					})
+				}
+			})
+		}
+	} catch (error) {
+		context.log('error occured here', error)
+	}
+
+	context.log('JavaScript timer trigger function ran!', timeStamp)
+}
--- NewsCrawlerTrigger/linkCrawler.js
+++ NewsCrawlerTrigger/linkCrawler.js
@@ -1,132 +1,137 @@
-const puppeteer = require("puppeteer");
-process.setMaxListeners(Infinity);
-const { newsPortalLink } = require("../constants/portal");
-const { KANTIPUR, SETOPATI, RATOPATI, DAINIK_KHABAR } = newsPortalLink;
+const puppeteer = require('puppeteer')
+process.setMaxListeners(Infinity)
+const { newsPortalLink } = require('../constants/portal')
+const { KANTIPUR, SETOPATI, RATOPATI, DAINIK_KHABAR } = newsPortalLink
 
 const scrapeNewsLink = (baseUrl, url) => {
-  switch (baseUrl) {
-    case KANTIPUR:
-      return scrapeKantipurNewsLink(url);
-    case SETOPATI:
-      return scrapeSetoPatiLink(url);
-    case RATOPATI:
-      return scrapeRatoPatiLink(url);
-    case DAINIK_KHABAR:
-      return scrapeDainikNepalLinks(url);
-    default:
-      return null;
-  }
-};
+	switch (baseUrl) {
+		case KANTIPUR:
+			return scrapeKantipurNewsLink(url)
+		case SETOPATI:
+			return scrapeSetoPatiLink(url)
+		case RATOPATI:
+			return scrapeRatoPatiLink(url)
+		case DAINIK_KHABAR:
+			return scrapeDainikNepalLinks(url)
+		default:
+			return {
+				error: {
+					status: true
+				},
+				data: null
+			}
+	}
+}
 
 const scrapeKantipurNewsLink = async url => {
-  try {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(url, { timeout: 0 });
+	try {
+		const browser = await puppeteer.launch()
+		const page = await browser.newPage()
+		await page.goto(url, { timeout: 0 })
 
-    const scrapedData = await page.evaluate(() =>
-      Array.from(document.querySelectorAll("article > h1, h2 > a "))
-        .slice(0, 1)
-        .map(link => ({
-          url: `https://ekantipur.com${link.getAttribute("href")}`
-        }))
-    );
-    await page.close();
-    await browser.close();
-    return { error: false, data: scrapedData };
-  } catch (err) {
-    return {
-      error: {
-        status: true,
-        stack: err
-      },
-      data: null
-    };
-  }
-};
+		const scrapedData = await page.evaluate(() =>
+			Array.from(document.querySelectorAll('article > h1, h2 > a '))
+				.slice(0, 1)
+				.map(link => ({
+					url: `https://ekantipur.com${link.getAttribute('href')}`
+				}))
+		)
+		await page.close()
+		await browser.close()
+		return { error: false, data: scrapedData }
+	} catch (err) {
+		return {
+			error: {
+				status: true,
+				stack: err
+			},
+			data: null
+		}
+	}
+}
 const scrapeSetoPatiLink = async url => {
-  try {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(url, { timeout: 0 });
+	try {
+		const browser = await puppeteer.launch()
+		const page = await browser.newPage()
+		await page.goto(url, { timeout: 0 })
 
-    const scrapedData = await page.evaluate(() =>
-      Array.from(document.querySelectorAll(".big-feature > a, .items > a "))
-        .slice(0, 1)
-        .map(link => ({
-          url: link.getAttribute("href")
-        }))
-    );
+		const scrapedData = await page.evaluate(() =>
+			Array.from(document.querySelectorAll('.big-feature > a, .items > a '))
+				.slice(0, 1)
+				.map(link => ({
+					url: link.getAttribute('href')
+				}))
+		)
 
-    await page.close();
-    await browser.close();
-    return { error: false, data: scrapedData };
-  } catch (err) {
-    return {
-      error: {
-        status: true,
-        stack: err
-      },
-      data: null
-    };
-  }
-};
+		await page.close()
+		await browser.close()
+		return { error: false, data: scrapedData }
+	} catch (err) {
+		return {
+			error: {
+				status: true,
+				stack: err
+			},
+			data: null
+		}
+	}
+}
 const scrapeRatoPatiLink = async url => {
-  try {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(url, { timeout: 0 });
+	try {
+		const browser = await puppeteer.launch()
+		const page = await browser.newPage()
+		await page.goto(url, { timeout: 0 })
 
-    const scrapedData = await page.evaluate(() =>
-      Array.from(document.querySelectorAll(".item-content  a"))
-        .slice(0, 1)
-        .map(link => ({
-          url: `https://ratopati.com${link.getAttribute("href")}`
-        }))
-    );
+		const scrapedData = await page.evaluate(() =>
+			Array.from(document.querySelectorAll('.item-content  a'))
+				.slice(0, 1)
+				.map(link => ({
+					url: `https://ratopati.com${link.getAttribute('href')}`
+				}))
+		)
 
-    await page.close();
-    await browser.close();
-    return { error: false, data: scrapedData };
-  } catch (err) {
-    return {
-      error: {
-        status: true,
-        stack: err
-      },
-      data: null
-    };
-  }
-};
+		await page.close()
+		await browser.close()
+		return { error: false, data: scrapedData }
+	} catch (err) {
+		return {
+			error: {
+				status: true,
+				stack: err
+			},
+			data: null
+		}
+	}
+}
 
 const scrapeDainikNepalLinks = async url => {
-  try {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(url, { timeout: 0 });
+	try {
+		const browser = await puppeteer.launch()
+		const page = await browser.newPage()
+		await page.goto(url, { timeout: 0 })
 
-    const scrapedData = await page.evaluate(() =>
-      Array.from(document.querySelectorAll(".news_loop a"))
-        .slice(0, 1)
-        .map(link => ({
-          url: link.getAttribute("href")
-        }))
-    );
+		const scrapedData = await page.evaluate(() =>
+			Array.from(document.querySelectorAll('.news_loop a'))
+				.slice(0, 1)
+				.map(link => ({
+					url: link.getAttribute('href')
+				}))
+		)
 
-    await page.close();
-    await browser.close();
-    return { error: false, data: scrapedData };
-  } catch (err) {
-    return {
-      error: {
-        status: true,
-        stack: err
-      },
-      data: null
-    };
-  }
-};
+		await page.close()
+		await browser.close()
+		return { error: false, data: scrapedData }
+	} catch (err) {
+		return {
+			error: {
+				status: true,
+				stack: err
+			},
+			data: null
+		}
+	}
+}
 
 module.exports = {
-  scrapeNewsLink
-};
+	scrapeNewsLink
+}
