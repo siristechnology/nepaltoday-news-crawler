9c06a8e0f41b25a7541ed03d92639984e5721c81
Shrijan Tripathi <shrijantripathi@Shrijans-MacBook-Pro.local>
2019-11-14 13:15:51 +0545

    NEP-128/create-notification-azure-function: todo for notification added

--- NotificationTrigger/index.js
+++ NotificationTrigger/index.js
@@ -1,9 +1,34 @@
-module.exports = async function (context, myTimer) {
-    var timeStamp = new Date().toISOString();
-    
-    if (myTimer.IsPastDue)
-    {
-        context.log('JavaScript is running late!');
-    }
-    context.log('JavaScript timer trigger function ran!', timeStamp);   
-};
+module.exports = async function(context, myTimer) {
+	const moment = require('moment-timezone')
+	const timeStamp = new Date().toISOString()
+	const { userDbService } = require('nepaltoday-db-service')
+
+	if (myTimer.IsPastDue) {
+		context.log('________________JavaScript is running late!_______________')
+	}
+
+	try {
+		const users = await userDbService.getUsers()
+		if (users) {
+			const userWithCurrentTime = users.map(user => {
+				const currentTime = moment()
+					.tz(user.timeZone)
+					.format('HH:mm')
+
+				return {
+					...user,
+					currentTime
+				}
+			})
+			console.log('_____________userWithCurrentTime__________', userWithCurrentTime)
+			if (userWithCurrentTime) {
+				// Todo: get the latest news from db
+				// check if time is 9pm if it is send notification using firebase api
+			}
+		}
+	} catch (error) {
+		console.log('_____________error__________', error)
+	}
+
+	context.log('JavaScript timer trigger function ran!', timeStamp)
+}
