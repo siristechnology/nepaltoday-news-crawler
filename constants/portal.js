ee7d6fdfbce60751ec4253c9211d33b849a7d721
Suraj Shrestha <syuraj@gmail.com>
2020-01-03 22:43:45 -0500

    W4 adding new source

--- constants/portal.js
+++ constants/portal.js
@@ -2,7 +2,8 @@ const newsPortalLink = {
 	KANTIPUR: 'https://ekantipur.com',
 	RATOPATI: 'https://ratopati.com',
 	SETOPATI: 'https://setopati.com',
-	DAINIK_KHABAR: 'https://dainiknepal.com'
+	DAINIK_KHABAR: 'https://dainiknepal.com',
+	ONLINE_KHABAR: 'https://onlinekhabar.com'
 }
 
 module.exports = {
